eb6da9a5f50399f7d6430f04997658938b634d94
Andres Orozco <andres.orozco97@hotmail.com>
2023-06-13 13:18:12 -0400

    change host app

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ app.include_router(router)
 Base.metadata.create_all(bind=engine)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
