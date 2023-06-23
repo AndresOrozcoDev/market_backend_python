64f7ce3a1ebd455f74b5217a7b3d24c1084fcbb0
Andres Orozco <andres.orozco97@hotmail.com>
2023-06-22 20:01:10 -0400

    async

--- services/user.py
+++ services/user.py
@@ -6,9 +6,9 @@ class UserService():
     def __init__(self, db) -> None:
         self.db = db
 
-    def post_forgetPassword(self, email: str):
-        new_password = generate_password()
+    async def post_forgetPassword(self, email: str):
+        new_password = await generate_password()
         subject = 'New Password'
         message = 'Your new password is: '
-        result = send_email(email, subject, message, new_password)
+        result = await send_email(email, subject, message, new_password)
         return result
